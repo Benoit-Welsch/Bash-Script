503901b24e22e0b34288003fc8eb5a0dc201ec21
Benoit-Welsch <benoitwelsch@protonmail.com>
2022-04-29 23:13:24 +0200

    🦄 ZFS : Run SMART test on all drive from a specific pool

--- zfs/Readme.md
+++ zfs/Readme.md
@@ -5,24 +5,19 @@
 Run Scrub on all zfs pool
 
 ```bash
-# Add execution permission
-chmod +x scrub_zfs.sh
-
 # Run scrub on all pool
-./scrub_zfs.sh
+sh scrub_zfs.sh
 ```
 
 ## S.M.A.R.T
 
-Run SMART Self-test on all drive used by zfs
+Run SMART Self-test on drive used by zfs
 
 ```bash
-# Add execution permission
-chmod +x smart_zfs.sh
-
-# Run short test
-./smart_zfs.sh short
-# OR
-# Run long test
-./smart_zfs.sh long
+# Run short test on all drive used by zfs pool
+sh smart_zfs.sh short
+# Run long test on all drive used by zfs pool
+sh smart_zfs.sh long
+# Run short test on all drive from a specific pool
+sh smart_zfs.sh short backup_tank
 ```
--- zfs/smart_zfs.sh
+++ zfs/smart_zfs.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
-if !  [[ $1 = "short" || $1 = "long" ]]; then
+set -e
+
+function onePool() {
+  zpool list $1
+  for disk in $(zdb -C $1 | grep -Po '(?<=[ *]path: ).+'); do
+    smartctl -t $1 $disk
+  done
+}
+
+function allDrive() {
+  for disk in $(ls /dev/disk/by-vdev/ | grep -E -o '(?<=\s+|^)[a-zA-Z0-9]+(?=\s+|$)'); do
+    smartctl -t $1 /dev/disk/by-vdev/$disk
+  done
+}
+
+if ! [[ $1 = "short" || $1 = "long" ]]; then
   echo "[Error] : Please provide the type of the smart test (short, long)"
   exit 1
 fi
 
-for disk in $(ls /dev/disk/by-vdev/ | grep -E -o '(?<=\s+|^)[a-zA-Z0-9]+(?=\s+|$)'); do
-  smartctl -t $1 /dev/disk/by-vdev/$disk
-done
+if [[ -n $2 ]]; then
+  onePool $2
+else
+  allDrive $1
+fi
